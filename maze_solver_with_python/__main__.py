83b8ff77bef3fcd3ff38620cc72ab634f1df34c6
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2025-05-04 12:43:19 +0200

    ✨ Add Cell class.

--- maze_solver_with_python/__main__.py
+++ maze_solver_with_python/__main__.py
@@ -1,6 +1,6 @@
 """Main program."""
 
-from maze_solver_with_python.core.models import Line, Point, Window
+from maze_solver_with_python.core.models import Cell, Point, Window
 
 
 def main() -> None:
@@ -9,7 +9,16 @@ def main() -> None:
     p1 = Point(100, 100)
     p2 = Point(200, 200)
 
-    win.draw_line(Line(p1, p2), "red")
+    cell1 = Cell(win, p1, p2, has_right_wall=False)
+    cell1.draw()
+
+    p3 = Point(200, 100)
+    p4 = Point(300, 200)
+    cell2 = Cell(win, p3, p4, has_left_wall=False, has_right_wall=False)
+    cell2.draw()
+
+    cell1.draw_move(cell2)
+
     win.wait_for_close()
 
 
--- maze_solver_with_python/core/models.py
+++ maze_solver_with_python/core/models.py
@@ -1,5 +1,6 @@
 """Module defining maze models."""
 
+import typing as tp
 from tkinter import BOTH, Canvas, Tk
 
 
@@ -55,3 +56,65 @@ class Window:
     def draw_line(self, line: Line, fill_color: str = "black") -> None:
         """Draw a line in window canvas."""
         line.draw(self.__canvas, fill_color)
+
+
+class Cell:
+    """Define a cell."""
+
+    def __init__(
+        self,
+        w: Window,
+        top_left: Point,
+        right_bottom: Point,
+        **kwargs: bool,
+    ) -> None:
+        self.configs = {}
+        self.configs["has_left_wall"] = kwargs.pop("has_left_wall", True)
+        self.configs["has_right_wall"] = kwargs.pop("has_right_wall", True)
+        self.configs["has_top_wall"] = kwargs.pop("has_top_wall", True)
+        self.configs["has_bottom_wall"] = kwargs.pop("has_bottom_wall", True)
+
+        self._x1 = top_left.x
+        self._y1 = top_left.y
+        self._x2 = right_bottom.x
+        self._y2 = right_bottom.y
+
+        self._w = w
+
+    def draw(self) -> None:
+        """Draw a cell."""
+        if self.configs["has_left_wall"]:
+            self._w.draw_line(
+                Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            )
+
+        if self.configs["has_right_wall"]:
+            self._w.draw_line(
+                Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            )
+
+        if self.configs["has_top_wall"]:
+            self._w.draw_line(
+                Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+            )
+
+        if self.configs["has_bottom_wall"]:
+            self._w.draw_line(
+                Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+            )
+
+    @property
+    def center(self) -> Point:
+        """Return the center point of the cell."""
+        return Point(
+            int((self._x2 + self._x1) / 2),
+            int((self._y2 + self._y1) / 2),
+        )
+
+    def draw_move(self, to_cell: tp.Self, undo: bool = False) -> None:
+        """Draw a move."""
+        fill_color = "red"
+        if undo:
+            fill_color = "grey"
+
+        self._w.draw_line(Line(self.center, to_cell.center), fill_color)
